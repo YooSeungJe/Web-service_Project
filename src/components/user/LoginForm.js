2c22ebf171dfd976020fa95b0a7a17cea5de633e
[AI7] 유승제 <dbtmdwp3562@gmail.com>
2023-05-02 22:06:15 +0900

    css통일

--- src/components/user/LoginForm.js
+++ src/components/user/LoginForm.js
@@ -1,6 +1,6 @@
 import React, { useState, useContext, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import styled from 'styled-components';
+
 import * as Api from '../../api';
 
 
@@ -10,106 +10,7 @@ import './LoginForm.css'
 import { DispatchContext, UserStateContext } from '../../App';
 
 
-// const Container = styled.div`
-//   min-height: 100vh;
-//   display: flex;
-//   justify-content: center;
-//   align-items: center;
-//   background-color : #FEFAE0;
-//   margin-top:-70px;
-  
-// `;
 
-// const LeftBox = styled.div`
-//   background-color: #02343F;
-//   padding: 2rem;
-//   border-radius: 7px;
-//   box-shadow: 0 20px 50px rgba(0, 0, 0, 0.1);
-//   width: 500px;
-  
-//   margin-top:100px;
-//   box-sizing:content-box;
-//   height:550px;
-  
-  
-  
-// `;
-
-
-// const RightBox = styled.div`
-//   background-color: #E9EDC9 ;
-//   padding: 2rem;
-//   border-radius: 7px;
-//   box-shadow: 0 20px 50px rgba(0, 0, 0, 0.1);
-//   width: 100%;
-//   max-width: 370px;
-//   margin-top:100px;
-//   box-sizing:content-box;
-//   height:550px;
-  
-// `;
-
-
-// const Heading = styled.h2`
-//   text-align: center;
-//   margin-bottom: 2rem;
-  
-// `;
-
-// const FormGroup = styled.div`
-//   margin-bottom: 1.5rem;
-// `;
-
-// const Label = styled.label`
-//   font-weight: bold;
-//   display: block;
-//   margin-bottom: 0.5rem;
-  
-// `;
-
-// const Input = styled.input`
-//   display: block;
-//   width: 100%;
-//   padding: 0.5rem;
-//   border: 1px solid #ccc;
-//   border-radius: 5px;
-//   font-size: 1rem;
-  
-// `;
-
-// const ErrorMessage = styled.p`
-//   color: #FE6244;
-//   font-size: 0.875rem;
-//   margin-top: 0.25rem;
-// `;
-
-// const Button = styled.button`
-  
-//   margin-top:30px;
-//   font-size: 1rem;
-//   padding: 0.75rem 1rem;
-//   border-radius: 5px;
-//   border: none;
-//   width: 100%;
-//   margin-bottom: 1rem;
-//   cursor: pointer;
-//   transition: background-color 0.2s ease-in-out;
-
-//   &:hover {
-//     background-color: #A4D0A4;
-//   }
-// `;
-
-// const SecondaryButton = styled(Button)`
-  
-//   color: #02343F;
-//   margin-top:9px;
-  
-//   &:hover {
-//     background-color: #F0EDCC;
-    
-//   }
-// `;
 function LoginForm() {
   const navigate = useNavigate();
   const userState = useContext(UserStateContext);
