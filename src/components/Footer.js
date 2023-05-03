43f46b712223e30c0665125aa8edae315fa6fc7d
PARK JAEHA <parkjaeha97@gmail.com>
2023-05-03 21:01:17 +0900

    (chore): Footer 스타일컴포넌트에서 css로 변경

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,37 +1,23 @@
-import styled from "styled-components";
-
-const Foot = styled.div`
-  width: 100%;
-  align-items: left;
-  background-color: #e9edc98c;
-  padding: 1rem;
-`;
-
-const FootTitle = styled.div`
-  display: inline-block;
-  font-weight: bold;
-  font-size: 1rem;
-`;
-
-const FootItem = styled.div`
-  display: inline-block;
-  font-size: 0.7rem;
-`;
+import './Footer.css';
 
 function Footer() {
   return (
-    <Foot>
-      <FootTitle>주식회사 잊지마 기억해</FootTitle>
+    <div id="footer">
+      <div className="footerTitle">주식회사 잊지마 기억해</div>
       <br />
-      <FootItem>대표 | 박민수</FootItem>
+      <div className="footerItem">대표 | 박민수</div>
       <br />
-      <FootItem>주소 | 대한민국 사랑시 고백구 행복동 14-72</FootItem>
+      <div className="footerItem">
+        주소 | 대한민국 사랑시 고백구 행복동 14-72
+      </div>
       <br />
-      <FootItem>전화 | 02-0000-0000 문의 | 1@elice.kr</FootItem>
+      <div className="footerItem">전화 | 02-0000-0000 문의 | 1@elice.kr</div>
       <br />
       <br />
-      <FootItem>Copyright 2023. 잊지마 기억해. All rights reserved.</FootItem>
-    </Foot>
+      <div className="footerItem">
+        Copyright 2023. 잊지마 기억해. All rights reserved.
+      </div>
+    </div>
   );
 }
 
