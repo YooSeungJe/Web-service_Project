b7791aaf3c8441cd8d230b3f0f24d249ee9fb042
[AI7/2반] 손태린 <elice.str@gmail.com>
2023-05-05 00:13:14 +0900

    (fix): image upload result message

--- src/components/image/UploadImage.js
+++ src/components/image/UploadImage.js
@@ -9,8 +9,11 @@ const UploadImage = ({ userId, dataId }) => {
     formData.append('dataId', dataId);
     const [ imageKey, setImageKey ] = useState(0);
     const isDeleted = imageKey === -1;
+    const isUploaded = imageKey > 0;
+
     const handleFileSelect = async (e) => {
       const image = e.target.files[0];
+
       if(image.size > 5 * 1024 * 1024){
         alert('업로드 가능한 최대 용량은 5MB입니다. ');
         return;
@@ -27,6 +30,7 @@ const UploadImage = ({ userId, dataId }) => {
 
     const handleFileDelete = async (e) => {
       e.preventDefault();
+      setImageKey(0)
       if(window.confirm('사진을 지우시겠습니까?')){
         try{
           const res = await Api.delete('image',dataId);
@@ -34,7 +38,7 @@ const UploadImage = ({ userId, dataId }) => {
         }catch(error){
           console.log(error.response);
           if(error.response.data==='Not found'){
-            alert('사진이 등록되지 않았습니다.');
+            alert('사진이 등록되어 있지 않습니다.');
           }else{
             alert('사진이 삭제되지 않았습니다. 다시 시도해주세요.');
           }
@@ -45,9 +49,10 @@ const UploadImage = ({ userId, dataId }) => {
     return (
       <Container>
         <ShowImage userId={userId} dataId={dataId} imageKey={imageKey}/>
-        {isDeleted && <p className="small text-danger">사진이 삭제되었습니다.</p>}
+        {isUploaded && <p className="small text-danger text-center">사진이 등록되었습니다.</p>}
+        {isDeleted && <p className="small text-danger text-center">사진이 삭제되었습니다.</p>}
         <Stack maxWidth='md' mb={1} justifyContent='space-evenly' direction="row" flexWrap="wrap" useFlexGap>
-          <Button variant="contained" component="label" startIcon={<PhotoCamera />}>
+          <Button variant="contained" component="label" startIcon={<PhotoCamera />} onClick={()=>setImageKey(0)}>
             Upload
             <input hidden accept="image/*" multiple type="file" onChange={handleFileSelect}/>
           </Button>
