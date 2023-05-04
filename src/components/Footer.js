ae007e03bc6404270c486dec4f5da4f428b61ebe
PARK JAEHA <parkjaeha97@gmail.com>
2023-05-05 00:56:03 +0900

    (fix): footer의 깨짐 수정

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,36 +6,25 @@ function Footer() {
     <div id="footer">
       <img className="turtle" src={turtle} alt="cute turtle" />
       <div id="footerContent">
-        <p className="footerTitle">주식회사 잊지마 기억해</p>
-        <p className="footerItem">
-          <span>
-            <b>대표 | </b>
-          </span>
-          <div>박민수</div>
-          <span>
-            <b>상임이사 | </b>
-          </span>
+        <div className="footerTitle">주식회사 잊지마 기억해</div>
+        <div className="footerItem">
+          <b>&nbsp;&nbsp;대표 | </b>
+          <div>박민수&nbsp;&nbsp;&nbsp;</div>
+          <b>상임이사 | </b>
           <div>Chat GPT</div>
-        </p>
-        <p className="footerItem">
-          <span>
-            <b>주소 | </b>
-          </span>
+          <br />
+          <b>&nbsp;&nbsp;주소 | </b>
           <div>대한민국 사랑시 고백구 행복동 14-72</div>
-        </p>
-        <p className="footerItem">
-          <span>
-            <b>전화 | </b>
-          </span>
-          <div>02-0000-0000</div>
-          <span>
-            <b>문의 | </b>
-          </span>
+          <br />
+          <b>&nbsp;&nbsp;전화 | </b>
+          <div>02-0000-0000&nbsp;&nbsp;&nbsp;</div>
+          <b>문의 | </b>
           <div>1@elice.kr</div>
-        </p>
-        <p className="footerCopyright">
-          Copyright 2023. 잊지마 기억해. All rights reserved.
-        </p>
+          <br />
+        </div>
+        <div className="footerCopyright">
+          &nbsp;&nbsp;Copyright 2023. 잊지마 기억해. All rights reserved.
+        </div>
       </div>
     </div>
   );
