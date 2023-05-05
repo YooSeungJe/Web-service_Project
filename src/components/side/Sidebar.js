172a0c24c68e66fc56f2f0fe3ccc52c3ab06a2ba
[AI7/2반] 손태린 <elice.str@gmail.com>
2023-05-05 14:42:28 +0900

    (feat): floating sidebar for scrolling

---
+++ src/components/side/Sidebar.js
@@ -0,0 +1,35 @@
+import React, { useRef, useState } from 'react';
+import './Sidebar.css';
+
+function Sidebar(pos){
+  const [ showBar, setShowBar ] = useState(true);
+  const position = pos.scrollElement.current;
+  const top = document.querySelector('#root');
+  const onMoveBox = (card) => {
+    setShowBar(!showBar)
+    if(card===0){
+      top.scrollIntoView({ behavior: "smooth", block: "start" });
+    }else{
+      position[card].scrollIntoView({ behavior: "smooth", block: "start" });
+    }
+  };
+
+  return (
+    <>
+      {showBar && 
+        <div className='sidebar'>
+          <div onClick={()=>onMoveBox(0)}>맨 위로</div>
+          <div onClick={()=>onMoveBox(1)}>학력</div>
+          <div onClick={()=>onMoveBox(2)}>프로젝트</div>
+          <div onClick={()=>onMoveBox(3)}>자격증</div>
+          <div onClick={()=>onMoveBox(4)}>수상경력</div>
+        </div>
+      }
+      <div className='sidebarButton'onClick={()=>setShowBar(!showBar)}>
+        이동
+      </div>
+    </>
+  )
+}
+
+export default Sidebar;
